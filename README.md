3b747d96156b80a9f67f8c923ec2bd96c2ff866f
Leszek Pietrzak <leszek@closbrothers.pl>
2014-10-01 12:45:42 +0200

    svg icon module, starting to fix color palette

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # GOG
+
+## Issues and notes
+
+Issues:
+
+- in the `.psd` mockup there was over 20 shades of gray used, I tried to bring this number down, but you are in need of a more precise color palette in your identity
+
+Notes:
+
+- the site is run on AngularJS with most content defined in `.json` data files
+- I decided to use [Attribute Modules CSS technique](http://amcss.github.io/) for modules
--- app/directives.js
+++ app/directives.js
@@ -7,3 +7,18 @@ gogApp.directive('bundlesCounter', function () {
         }
     };
 });
+
+// for svg icons with variables in attributes
+gogApp.directive('ngXlinkHref', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            // declarations
+            var attrName = 'xlink:href';
+            // observe attr change and set attribute if value exist
+            attr.$observe('ngXlinkHref', function (value) {
+                if (value) { attr.$set(attrName, value); }
+            });
+        }
+    };
+});
